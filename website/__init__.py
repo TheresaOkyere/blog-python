c05ee608441035f7d13d1ac24d4236dd83f37b11
code-to <Theresaokyere034@gmail.com>
2022-02-08 14:25:25 +0000

    some routes

--- website/__init__.py
+++ website/__init__.py
@@ -7,4 +7,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
+    @app.route("/")
+    def home():
+        return "<h1>Hello</h1>"
+
+
+    @app.route("/profile")
+    def profile():
+        return "<h1>Profile</h1>"
     return app
